4ad6a59f1856bb7bdb7a26cbb41aca7a78b8ea83
Gabriel Quina <gabriel.quina@ymail.com>
2024-02-14 01:14:03 -0300

    Lazy com plugins instalados - telescope, treesitter, neotree, lualine, catippuccin, lsp-config, mason

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require 'nvim-treesitter.install'.prefer_git = false
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {"lua", "javascript", "php", "python"},
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
