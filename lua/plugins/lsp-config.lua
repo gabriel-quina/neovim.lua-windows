8bd5c5d3e2b42bfb99e099ede59f22430cd0c8e8
Gabriel Quina <gabriel.quina@ymail.com>
2024-02-16 01:12:18 -0300

    Adicionado Zoxide e none-ls, configurado treesitter para autoinstall e fixes de formatação

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,8 +2,25 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup()
-    end
+      require("mason").setup({
+        PATH = "prepend",
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "intelephense" },
+      })
+    end,
   },
   {
     "neovim/nvim-lspconfig",
@@ -11,17 +28,10 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
+      lspconfig.intelephense.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" }
-      })
-    end
-    }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     require 'nvim-treesitter.install'.prefer_git = false
     local config = require("nvim-treesitter.configs")
     config.setup({
+      auto_install = true,
       ensure_installed = {"lua", "javascript", "php", "python", "vimdoc"},
       highlight = { enable = true },
       indent = { enable = true },
